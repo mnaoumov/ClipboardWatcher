f68f6ab14230d4072cc446eda14dbeb67fbafe74
Michael Naumov <michael.naumov@fiserv.com>
2013-08-26 15:07:20 +1000

    Remove copyright

--- ClipboardObserver/Properties/AssemblyInfo.cs
+++ ClipboardObserver/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JLorek.Samples.ClipboardObserver")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JLorek.Samples.ClipboardObserver")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
