3fbe046b078d4ed25fd785bd1142224e5bc49a86
Michael Naumov <michael.naumov@fiserv.com>
2013-08-26 15:12:34 +1000

    Improving User32

--- ClipboardObserver/User32.cs
+++ ClipboardObserver/User32.cs
@@ -5,19 +5,21 @@ namespace ClipboardObserver
 {
     class User32
     {
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private const string User32Dll = "User32.dll";
+
+        [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
 
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern bool AddClipboardFormatListener(IntPtr hWndObserver);
 
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern bool RemoveClipboardFormatListener(IntPtr hWndObserver);
 
         public enum Message
