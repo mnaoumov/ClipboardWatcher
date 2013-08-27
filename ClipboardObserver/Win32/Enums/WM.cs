a997d1d25f5470eb5fa1b25187bcca8839acbdbd
Michael Naumov <michael.naumov@fiserv.com>
2013-08-27 10:17:34 +1000

    Update Win32 comments

--- ClipboardObserver/Win32/Enums/WM.cs
+++ ClipboardObserver/Win32/Enums/WM.cs
@@ -1,7 +1,16 @@
 namespace ClipboardObserver.Win32.Enums
 {
+    /// <summary>
+    /// Clipboard Notifications
+    /// http://msdn.microsoft.com/en-us/library/windows/desktop/ff468804.aspx
+    /// Windows SDK - WinUser.h.
+    /// </summary>
     public enum WM
     {
+        /// <summary>
+        /// Sent when the contents of the clipboard have changed.
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms649021.aspx
+        /// </summary>
         WM_CLIPBOARDUPDATE = 0x031D
     }
 }
--- ClipboardObserver/Win32/Libs/User32.cs
+++ ClipboardObserver/Win32/Libs/User32.cs
@@ -12,12 +12,21 @@ namespace ClipboardObserver.Win32.Libs
 
         /// <summary>
         /// Places the given window in the system-maintained clipboard format listener list.
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms649033.aspx
+        /// Windows SDK - WinUser.h
         /// </summary>
         /// <param name="hWndObserver"></param>
         /// <returns></returns>
         [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern bool AddClipboardFormatListener(IntPtr hWndObserver);
 
+        /// <summary>
+        /// Removes the given window from the system-maintained clipboard format listener list.
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms649050.aspx
+        /// Windows SDK - WinUser.h
+        /// </summary>
+        /// <param name="hWndObserver"></param>
+        /// <returns></returns>
         [DllImport(User32Dll, CharSet = CharSet.Auto)]
         public static extern bool RemoveClipboardFormatListener(IntPtr hWndObserver);
     }
