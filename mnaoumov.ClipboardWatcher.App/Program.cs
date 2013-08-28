fb8ea7f763ddecaefdd6888621ad901453097a4c
Michael Naumov <michael.naumov@fiserv.com>
2013-08-29 09:19:53 +1000

    Remove namespace

--- mnaoumov.ClipboardWatcher.App/Program.cs
+++ mnaoumov.ClipboardWatcher.App/Program.cs
@@ -8,7 +8,7 @@ namespace mnaoumov.ClipboardWatcher.App
         {
             Console.WriteLine("Press [RETURN] to quit...");
 
-            using (var clipboardWatcher = new ClipboardWatcher())
+            using (var clipboardWatcher = new global::ClipboardWatcher())
             {
                 clipboardWatcher.ClipboardTextChanged += text => Console.WriteLine(string.Format("Text arrived @ clipboard: {0}", text));
                 Console.ReadLine();
--- mnaoumov.ClipboardWatcher/ClipboardWatcher.cs
+++ mnaoumov.ClipboardWatcher/ClipboardWatcher.cs
@@ -4,114 +4,111 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace mnaoumov.ClipboardWatcher
+public class ClipboardWatcher : IDisposable
 {
-    public class ClipboardWatcher : IDisposable
+    readonly Thread _formThread;
+    bool _disposed;
+
+    public ClipboardWatcher()
     {
-        readonly Thread _formThread;
-        bool _disposed;
+        _formThread = new Thread(() => { new ClipboardWatcherForm(this); })
+                      {
+                          IsBackground = true
+                      };
 
-        public ClipboardWatcher()
+        _formThread.SetApartmentState(ApartmentState.STA);
+        _formThread.Start();
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+        Disposed();
+        if (_formThread != null && _formThread.IsAlive)
+            _formThread.Abort();
+        _disposed = true;
+        GC.SuppressFinalize(this);
+    }
+
+    ~ClipboardWatcher()
+    {
+        Dispose();
+    }
+
+    public event Action<string> ClipboardTextChanged = delegate { };
+    public event Action Disposed = delegate { };
+
+    public void OnClipboardTextChanged(string text)
+    {
+        ClipboardTextChanged(text);
+    }
+}
+
+public class ClipboardWatcherForm : Form
+{
+    public ClipboardWatcherForm(ClipboardWatcher clipboardWatcher)
+    {
+        HideForm();
+        RegisterWin32();
+        ClipboardTextChanged += clipboardWatcher.OnClipboardTextChanged;
+        clipboardWatcher.Disposed += () => Invoke(new Action(Dispose));
+        Disposed += (sender, args) => UnregisterWin32();
+        Application.Run(this);
+    }
+
+    public event Action<string> ClipboardTextChanged = delegate { };
+
+    void HideForm()
+    {
+        FormBorderStyle = FormBorderStyle.None;
+        ShowInTaskbar = false;
+        Load += (sender, args) => { Size = new Size(0, 0); };
+    }
+
+    void RegisterWin32()
+    {
+        User32.AddClipboardFormatListener(Handle);
+    }
+
+    void UnregisterWin32()
+    {
+        User32.RemoveClipboardFormatListener(Handle);
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        switch ((WM)m.Msg)
         {
-            _formThread = new Thread(() => { new ClipboardWatcherForm(this); })
-                          {
-                              IsBackground = true
-                          };
+            case WM.WM_CLIPBOARDUPDATE:
+                ClipboardChanged();
+                break;
 
-            _formThread.SetApartmentState(ApartmentState.STA);
-            _formThread.Start();
-        }
-
-        public void Dispose()
-        {
-            if (_disposed)
-                return;
-            Disposed();
-            if (_formThread != null && _formThread.IsAlive)
-                _formThread.Abort();
-            _disposed = true;
-            GC.SuppressFinalize(this);
-        }
-
-        ~ClipboardWatcher()
-        {
-            Dispose();
-        }
-
-        public event Action<string> ClipboardTextChanged = delegate { };
-        public event Action Disposed = delegate { };
-
-        public void OnClipboardTextChanged(string text)
-        {
-            ClipboardTextChanged(text);
+            default:
+                base.WndProc(ref m);
+                break;
         }
     }
 
-    public class ClipboardWatcherForm : Form
+    void ClipboardChanged()
     {
-        public ClipboardWatcherForm(ClipboardWatcher clipboardWatcher)
-        {
-            HideForm();
-            RegisterWin32();
-            ClipboardTextChanged += clipboardWatcher.OnClipboardTextChanged;
-            clipboardWatcher.Disposed += () => Invoke(new Action(Dispose));
-            Disposed += (sender, args) => UnregisterWin32();
-            Application.Run(this);
-        }
-
-        public event Action<string> ClipboardTextChanged = delegate { };
-
-        void HideForm()
-        {
-            FormBorderStyle = FormBorderStyle.None;
-            ShowInTaskbar = false;
-            Load += (sender, args) => { Size = new Size(0, 0); };
-        }
-
-        void RegisterWin32()
-        {
-            User32.AddClipboardFormatListener(Handle);
-        }
-
-        void UnregisterWin32()
-        {
-            User32.RemoveClipboardFormatListener(Handle);
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            switch ((WM) m.Msg)
-            {
-                case WM.WM_CLIPBOARDUPDATE:
-                    ClipboardChanged();
-                    break;
-
-                default:
-                    base.WndProc(ref m);
-                    break;
-            }
-        }
-
-        void ClipboardChanged()
-        {
-            if (Clipboard.ContainsText())
-                ClipboardTextChanged(Clipboard.GetText());
-        }
+        if (Clipboard.ContainsText())
+            ClipboardTextChanged(Clipboard.GetText());
     }
+}
 
-    public enum WM
-    {
-        WM_CLIPBOARDUPDATE = 0x031D
-    }
+public enum WM
+{
+    WM_CLIPBOARDUPDATE = 0x031D
+}
 
-    public class User32
-    {
-        const string User32Dll = "User32.dll";
+public class User32
+{
+    const string User32Dll = "User32.dll";
 
-        [DllImport(User32Dll, CharSet = CharSet.Auto)]
-        public static extern bool AddClipboardFormatListener(IntPtr hWndObserver);
+    [DllImport(User32Dll, CharSet = CharSet.Auto)]
+    public static extern bool AddClipboardFormatListener(IntPtr hWndObserver);
 
-        [DllImport(User32Dll, CharSet = CharSet.Auto)]
-        public static extern bool RemoveClipboardFormatListener(IntPtr hWndObserver);
-    }
+    [DllImport(User32Dll, CharSet = CharSet.Auto)]
+    public static extern bool RemoveClipboardFormatListener(IntPtr hWndObserver);
 }
